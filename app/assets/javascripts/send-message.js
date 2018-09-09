e84eca014316c0814bbde69d3b0d8308e3fed896
Fumie Toyooka <dymojet@gmail.com>
2018-09-09 16:21:43 +0900

    add ajax with formData

--- app/assets/javascripts/send-message.js
+++ app/assets/javascripts/send-message.js
@@ -1,5 +1,13 @@
 $(function () {
-  $("form").on("submit", function () {
-    console.log("フォームが発火")
+  $('form').on('submit', function () {
+    var form = document.querySelector('form')
+    var formData = new FormData(form)
+    var groupId = $('.form__submit').data('group-id')
+    $.ajax({
+      type: 'POST',
+      url: '/groups/' + groupId + '/messages',
+      data: formData,
+      dataType: 'json'
+    })
   })
 })
