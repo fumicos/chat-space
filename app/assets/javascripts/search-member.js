47220f291f93bce6da02671d79d94e3e8dd76ba1
Fumie Toyooka <dymojet@gmail.com>
2018-10-04 13:15:55 +0900

    Fix review

--- app/assets/javascripts/search-member.js
+++ app/assets/javascripts/search-member.js
@@ -1,17 +1,18 @@
 $(function () {
   function buildHTML(user) {
-    if (!user) {
-      var html = `
-        <div class="chat-group-user clearfix">
-          <p>該当するユーザはいません。</p>
-        </div>
-      `
-    } else {
-      var html = `
+    var html
+    if (user) {
+      html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
         <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
       </div>
+      `
+    } else {
+      html = `
+        <div class="chat-group-user clearfix">
+          <p>該当するユーザはいません。</p>
+        </div>
       `
     }
     return html
